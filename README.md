422d306361fee3eac43f68fd49189708393a4618
Eric Pierpont <epierpont@swiftcom.com>
2018-01-29 22:11:45 -0500

    Added main plugin file and changed readme title

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# WP-dev-site-lockout
-WP Dev Site Lockout is used to hide your dev site from web crawlers and the general public. 
+# WP Dev Site Lockout
+WP Dev Site Lockout is used to hide your dev site from web crawlers and the general public.
---
+++ wp-dev-site-lockout.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: WP Dev Site Lockout
+Plugin URI: http://www.ericpierpont.com/
+Description: Hide your dev site from web crawlers and the general public.
+Version: 0.1
+Author: Eric Pierpont
+Author URI: http://www.ericpierpont.com/
+License: GPL
+Copyright: Eric Pierpont
+Text Domain: wpdsl
+*/
